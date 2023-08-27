e8a574d1b4c8735466774004e071b3136a220285
Goran <goran.rihter@gmail.com>
2023-08-27 20:07:53 +0200

    config corrections, added shadcn (button)

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 const App = () => {
     return (
-        <>
-            <div className="h-screen w-screen bg-gray-100">placeholder</div>
-        </>
+        <div className="flex flex-col items-center justify-center gap-2">
+            <h1 className="text-3xl font-bold">
+                Vite + React + TypeScript + Shadcn UI
+            </h1>
+            <Button>Shadcn UI Button</Button>
+            <Button variant="outline">Shadcn UI Button</Button>
+        </div>
     );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import path from 'path';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    base: '/frontend-mentor/',
     plugins: [react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
-    base: '/frontend-mentor/',
 });
